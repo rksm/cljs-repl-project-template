4a8f6abadfe8e5a8a36b92ef3e695d07f0774ab6
Robert Krahn <robert.krahn@gmail.com>
2014-08-10 00:58:15 -0700

    adding cljs tests

--- src/leiningen/new/cljs_repl_project.clj
+++ src/leiningen/new/cljs_repl_project.clj
@@ -11,19 +11,21 @@
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' cljs-repl-project project.")
 
-    (->files data
-             [".gitignore" (render ".gitignore" data)]
-             ["LICENSE" (render "LICENSE" data)]
-             ["README.md" (render "README.md" data)]
-             ["doc/intro.md" (render "doc/intro.md" data)]
-             ["project.clj" (render "project.clj" data)]
-             ["src/cljs/{{sanitized}}/core.cljs" (render "cljs/core.cljs" data)]
-             ["src/clj/{{sanitized}}/core.clj" (render "clj/core.clj" data)]
-             ["src/clj/{{sanitized}}/routes.clj" (render "clj/routes.clj" data)]
-             ["test/clj/{{sanitized}}/core_test.clj" (render "test/core_test.clj" data)]
-             ["resources/public/example.html" (render "resources/public/example.html" data)]
-             ["scripts/start-web-server.sh" (render "scripts/start-web-server.sh" data) :executable true]
-             ["scripts/start-repl-server.sh" (render "scripts/start-repl-server.sh" data) :executable true])))
+    (->files
+     data
+     [".gitignore"                             (render ".gitignore" data)]
+     ["LICENSE"                                (render "LICENSE" data)]
+     ["README.md"                              (render "README.md" data)]
+     ["doc/intro.md"                           (render "doc/intro.md" data)]
+     ["project.clj"                            (render "project.clj" data)]
+     ["src/cljs/{{sanitized}}/core.cljs"       (render "cljs/core.cljs" data)]
+     ["src/clj/{{sanitized}}/core.clj"         (render "clj/core.clj" data)]
+     ["src/clj/{{sanitized}}/routes.clj"       (render "clj/routes.clj" data)]
+     ["test/clj/{{sanitized}}/core_test.clj"   (render "test/clj/core_test.clj" data)]
+     ["test/cljs/{{sanitized}}/core_test.cljs" (render "test/cljs/core_test.cljs" data)]
+     ["resources/public/example.html"          (render "resources/public/example.html" data)]
+     ["scripts/start-web-server.sh"            (render "scripts/start-web-server.sh" data) :executable true]
+     ["scripts/start-repl-server.sh"           (render "scripts/start-repl-server.sh" data) :executable true])))
 
 
 ;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
